4f17b66113724d797e4b56aadd24e022f86e108c
Joel Palmer <joelpalmer@users.noreply.github.com>
2020-09-03 09:49:37 -0500

    feat: add schema for Team

--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -6,7 +6,32 @@ type Player {
   position: String
 }
 
+type Team {
+  id: ID!
+  fullTeamName: String
+  venue: Venue
+  division: Division
+  league: League
+}
+
+type Venue {
+  id: ID!
+  name: String
+}
+
+type Division {
+  id: ID!
+  name: String
+}
+
+type League {
+  id: ID!
+  name: String
+}
+
+
 type Query {
   players: [Player]
+  team: Team
 }
 `
